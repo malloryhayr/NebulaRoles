0c2cad5e371e4a24d5f06360313b7e52bba8fa0d
iGalaxyYT <contact@igalaxy.dev>
2021-03-04 19:43:27 -0800

    Refactor TaskPatch

--- NebulaRoles/TaskPatch.cs
+++ NebulaRoles/TaskPatch.cs
@@ -9,15 +9,18 @@ namespace NebulaRoles
     {
         static void Postfix(PlayerControl __instance)
         {
-            System.Console.WriteLine(PlayerControl.LocalPlayer.GetModdedControl().Role);
-            if (PlayerControl.LocalPlayer != null)
+            if (Main.State.LocalPlayer != null)
             {
-                if (PlayerControl.LocalPlayer.IsPlayerRole("Jester"))
+                switch (Main.State.LocalPlayer.GetModdedControl().Role)
                 {
-                    ImportantTextTask ImportantTasks = new GameObject("JesterTasks").AddComponent<ImportantTextTask>();
-                    ImportantTasks.transform.SetParent(__instance.transform, false);
-                    ImportantTasks.Text = "[ED54BAFF]Get voted out to win.[]\nNo Tasks";
-                    __instance.myTasks.Insert(0, ImportantTasks);
+                    case "Jester":
+                    {
+                        ImportantTextTask ImportantTasks = new GameObject("JesterTasks").AddComponent<ImportantTextTask>();
+                        ImportantTasks.transform.SetParent(__instance.transform, false);
+                        ImportantTasks.Text = "[ED54BAFF]Get voted out to win.[]\nNo Tasks";
+                        __instance.myTasks.Insert(0, ImportantTasks);
+                        break;
+                    }
                 }
             }
         }
